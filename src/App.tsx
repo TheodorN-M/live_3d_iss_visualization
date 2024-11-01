f702716888945afa6b7c96578dbc8860f5ecec07
Theodor <theodornm@gmail.com>
2024-11-01 15:36:15 +0100

    Added live data fetching and update button

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const link = "https://api.wheretheiss.at/v1/satellites/25544";
+
+
+async function getISSLocation(){
+  let r = await (await fetch(link)).json();
+
+  console.log(r);
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getISSLocation}>Update!</button>
+
       </header>
     </div>
   );

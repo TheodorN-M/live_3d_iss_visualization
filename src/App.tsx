46b362f09e592d8cefd7531d110e1fd24ab86c4a
Theodor <theodornm@gmail.com>
2024-11-01 17:16:36 +0100

    Moved map logic to own .ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,34 @@
 import React, { useEffect, useRef } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import * as maptalks from 'maptalks';
+import {initializeMap, cleanupMap, updateMap} from './MapService';
 
-
-const link = "https://api.wheretheiss.at/v1/satellites/25544";
-
-async function getISSLocation(){
-  let r = await (await fetch(link)).json();
-
-  console.log(r);
-}
 
 function App() {
   const mapContainer = useRef<HTMLDivElement | null>(null); // Ref for the map container
-  const map = useRef<maptalks.Map | null>(null); // Ref for the map instance
 
   useEffect(() => {
-    if (mapContainer.current && !map.current) {
-      // Initialize the map only once when the component mounts
-      map.current = new maptalks.Map(mapContainer.current, {
-        center: [0, 0],
-        zoom: 2,
-        baseLayer: new maptalks.TileLayer('base', {
-          urlTemplate: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-          subdomains: ['a', 'b', 'c', 'd'],
-          attribution:
-            '&copy; <a href="http://www.osm.org/copyright">OSM</a> contributors, ' +
-            '&copy; <a href="https://carto.com/attributions">CARTO</a>',
-        }),
-      });
+    if (mapContainer.current) {
+      initializeMap(mapContainer.current);
     }
 
-    // Clean up map instance on component unmount
+    // Clean up the map on component unmount
     return () => {
-      if (map.current) {
-        map.current.remove();
-        map.current = null;
-      }
+      cleanupMap();
     };
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getISSLocation}>Update ISS Location</button>
-      </header>
-      <div
-        id="map"
-        ref={mapContainer}
-        style={{
-          width: '100%',
-          height: '500px',
+        <button onClick={updateMap}>Update ISS Location</button>
+        <div id="map"
+            ref={mapContainer}
+            style={{
+            width: '100%',
+            height: '500px',
         }}
-      ></div>
+        ></div>
+      </header>
     </div>
   );
 }
